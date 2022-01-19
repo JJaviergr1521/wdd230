99acba568f15e2e7dcdd8982f71499567b6e8eb8
JJaviergr1521 <jjaviergr@byui.edu>
2022-01-18 20:55:13 -0600

    ok

---
+++ week1/javascript/aboutme.js
@@ -0,0 +1,7 @@
+const date =  new Date();
+let actualY; 
+actualY = date.getFullYear();
+document.getElementById("year").innerHTML = actualY;
+document.getElementById("date").innerHTML = new Date();
+
+
